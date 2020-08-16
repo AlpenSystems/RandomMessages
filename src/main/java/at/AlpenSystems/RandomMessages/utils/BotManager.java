c918e6a34ad8a24f249b28a1e4e22beb2f35ee90
David Hülsberg <46696514+david-h-dev@users.noreply.github.com>
2020-08-16 06:52:05 +0200

    - BotManager //removed gateway intents which are unnecessary and were copied from another of my bots

--- src/main/java/at/AlpenSystems/RandomMessages/utils/BotManager.java
+++ src/main/java/at/AlpenSystems/RandomMessages/utils/BotManager.java
@@ -3,7 +3,7 @@
  * @author David-H-Dev
  *
  * last changes:
- * @date 15.08.2020
+ * @date 16.08.2020
  */
 
 package at.AlpenSystems.RandomMessages.utils;
@@ -13,26 +13,15 @@ import at.AlpenSystems.RandomMessages.listeners.OnMessage;
 import at.AlpenSystems.RandomMessages.utils.config.Config;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 import javax.security.auth.login.LoginException;
-import java.util.EnumSet;
 
 public class BotManager {
 
         public static void startBot() {
             try {
                 JDABuilder.createDefault(
-                        Config.get("TOKEN"),
-                        GatewayIntent.GUILD_MEMBERS,
-                        GatewayIntent.GUILD_MESSAGES,
-                        GatewayIntent.GUILD_VOICE_STATES,
-                        GatewayIntent.DIRECT_MESSAGES,
-                        GatewayIntent.GUILD_PRESENCES
-
-                ).disableCache(EnumSet.of(CacheFlag.CLIENT_STATUS, CacheFlag.ACTIVITY, CacheFlag.EMOTE)
-                ).enableCache(CacheFlag.VOICE_STATE
+                        Config.get("TOKEN")
 
                 //Listeners
                 ).addEventListeners(new OnMessage()
